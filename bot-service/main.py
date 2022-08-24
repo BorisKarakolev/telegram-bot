11830ecc6a5a8dde7c1f3334bea46c708f88d3aa
Boris Karakolev <boriskarakolev@hotmail.com>
2022-08-24 16:11:56 +0200

    Basic implementation of bot service

---
+++ bot-service/main.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("TELEGRAM_TOKEN")
+groups = ['-1437368496']
+msg = 'Hello there'
+
+# Fix token handle
+print(token)
+
+
+def send_msg(groups, msg):
+    if groups:
+        for group in groups:
+            try:
+                if not token:
+                    print('No token provided')
+                else:
+                    url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'.format(
+                        token, group, msg)
+                    res = requests.get(url)
+                    print(res.text)
+            except:
+                print('Something went wrong')
+    else:
+        print('Please provide some group ids')
+
+
+send_msg(groups, msg)
